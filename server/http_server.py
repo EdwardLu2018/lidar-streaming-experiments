373e00c9c1ca2b3bee3775ec9d7be6c82842cc77
Edward <lued2018@gmail.com>
2022-01-09 13:59:58 -0500

    start http server

---
+++ server/http_server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, Response
+from camera import Camera
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/lidarstream')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True)
